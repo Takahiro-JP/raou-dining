141c467a2eba898838ff1302c160f8443e1e4780
Takahiro-JP <matsumotokixt1@gmail.com>
2019-04-06 19:21:17 +0900

    新規作成

---
+++ raou_dining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  raou_dining
+//
+//  Created by 松本貴大 on 2019/04/06.
+//  Copyright © 2019 松本貴大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ raou_diningTests/raou_diningTests.swift
@@ -0,0 +1,34 @@
+//
+//  raou_diningTests.swift
+//  raou_diningTests
+//
+//  Created by 松本貴大 on 2019/04/06.
+//  Copyright © 2019 松本貴大. All rights reserved.
+//
+
+import XCTest
+@testable import raou_dining
+
+class raou_diningTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
